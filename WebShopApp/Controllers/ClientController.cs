23ade33821de192452de37dc4e1cf6fd76fef8d3
didkote <didostav@abv.bg>
2025-01-31 10:30:08 +0200

    ee

---
+++ WebShopApp/Controllers/ClientController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebShopApp.Infrastructure.Data.Domain;
+using WebShopApp.Models.Client;
+
+namespace WebShopApp.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ClientController(UserManager<ApplicationUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        // GET: ClientController
+        public async Task<IActionResult> Index()
+        {
+            var allUsers = this._userManager.Users.Select(x => new ClientIndexVM
+            {
+                Id = x.Id,
+                UserName = x.UserName,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Address = x.Address,
+                Email = x.Email,
+            }).ToList();
+
+            var adminIds = (await _userManager.GetUsersInRoleAsync("Administrator")).Select(x => x.Id).ToList();
+
+            foreach (var user in allUsers)
+            {
+                user.IsAdmin = adminIds.Contains(user.Id);
+            }
+
+            var users = allUsers.Where(x => x.IsAdmin == false).OrderBy(x => x.UserName).ToList();
+
+            return this.View(users);
+        }
+
+        // GET: ClientController/Delete/5
+        public ActionResult Delete(string id)
+        {
+            var user = this._userManager.Users.FirstOrDefault(x => x.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            ClientDeleteVM userToDelete = new ClientDeleteVM()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Address = user.Address,
+                Email = user.Email,
+                UserName = user.UserName,
+            };
+
+            return this.View(userToDelete);
+        }
+
+        // POST: ClientController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete (ClientDeleteVM bindingModel)
+        {
+            string id = bindingModel.Id;
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            IdentityResult result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return NotFound();
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebShopApp/Controllers/StatisticController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShopApp.Core.Contracts;
+using WebShopApp.Models.Statistic;
+
+namespace WebShopApp.Controllers
+{
+    public class StatisticController: Controller
+    {
+        private readonly IStatisticService statisticService;
+
+        public StatisticController(IStatisticService statisticService)
+        {
+            this.statisticService = statisticService;
+        }
+        public ActionResult Index()
+        {
+            StatisticVM statistics = new StatisticVM();
+
+            statistics.CountClients = statisticService.CountClients();
+            statistics.CountProducts = statisticService.CountProducts();
+            statistics.CountOrders = statisticService.CountOrders();
+            statistics.SumOrders = statisticService.SumOrders();
+            return View(statistics);
+
+        }
+
+    }
+}
